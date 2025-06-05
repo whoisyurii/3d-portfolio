824af7eadec229e71c1e24731b76ff71e63e601f
Yurii <yuriimashchuk@gmail.com>
2025-06-05 20:21:28 +0300

    feat: add Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <ExperienceSection />
       <TechStack />
       <Testimonials />
+      <Contact />
     </>
   );
 };
